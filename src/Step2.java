2fc388bb551aa60c31a5ae21905faec04feb8f2d
Tobie W <tobiewee@gmail.com>
2016-12-05 23:36:53 +0800

    Updated descriptions

--- src/Step2.java
+++ src/Step2.java
@@ -3,17 +3,18 @@
  * A quick introduction to conditionals and loops.
  *
  * What it covers:
- * - Hello World program.
- * - Output to console.
- * - Variable declaration.
- * - Variable initialization.
- * - Basic data types.
+ * - if..else..else if
+ * - switch
+ * - for loops
+ * - for each loops
+ * - while loops
+ * - do..while loops
  *
  * Created by Tobie Wee
  */
 
 public class Step2 {
     public static void main(String[] args) {
-        // Work in progress.
+        // Work in progress. Stay tuned for updates.
     }
 }
