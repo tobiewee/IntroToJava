a420b662f4b9569327af10228e7fd083a02f1b30
Tobie W <tobiewee@gmail.com>
2016-12-05 20:55:39 +0800

    Updated project

---
+++ src/Step2.java
@@ -0,0 +1,19 @@
+/*
+ * Step 2.
+ * A quick introduction to conditionals and loops.
+ *
+ * What it covers:
+ * - Hello World program.
+ * - Output to console.
+ * - Variable declaration.
+ * - Variable initialization.
+ * - Basic data types.
+ *
+ * Created by Tobie Wee
+ */
+
+public class Step2 {
+    public static void main(String[] args) {
+        // Work in progress.
+    }
+}
